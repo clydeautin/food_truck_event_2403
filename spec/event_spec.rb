2bbc4bc6255968dfb6dacd2e4fd6407a80bd66f7
Clyde Autin <clyde.autin@gmail.com>
2024-04-15 08:58:14 -0700

    test to add food trukcs created

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe Event do
             expect(event).to be_an_instance_of(Event)
         end
     end
+    describe 'managing food trucks' do
+        it 'can add a food truck' do
+            event = Event.new("South Pearl Street Farmers Market")
+            food_truck1 = FoodTruck.new("Rocky Mountain Pies")
+
+            event.add_food_truck(food_truck1)
+
+            expect(event.food_trucks).to include(food_truck1)
+        end
+    end
 end
