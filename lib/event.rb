4b5adbdffa25a73226cdd8ef45d8b735d15f7fd4
Clyde Autin <clyde.autin@gmail.com>
2024-04-15 09:20:15 -0700

    Event can now show trucks that have specific items

--- lib/event.rb
+++ lib/event.rb
@@ -14,4 +14,9 @@ class Event
     def food_truck_names
         names = food_trucks.map { |food_truck| food_truck.name }
     end
+
+    def food_trucks_that_sell(item)
+        trucks_with_item = food_trucks.select { |food_truck| food_truck.inventory.include?(item) }
+        trucks_with_item
+    end
 end
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -40,6 +40,7 @@ RSpec.describe Event do
             event.add_food_truck(food_truck1)
             event.add_food_truck(food_truck2)
             event.add_food_truck(food_truck3)
+            # binding.pry
 
             expect(event.food_trucks_that_sell(item1)).to include(food_truck1, food_truck3)
             expect(event.food_trucks_that_sell(item4)).to include(food_truck2)
